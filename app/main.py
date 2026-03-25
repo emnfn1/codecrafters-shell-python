5f768cb0baf0fa1beef6f574eab5895fff4d3023
Emin Furkan Safer <eminfurkan.safer@ogr.iu.edu.tr>
2026-03-25 22:12:42 +0300

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -13,7 +13,7 @@ builtin = {
 
 def custom_args(args):
     for arg in args:
-        path = shutil.which(user_input[1], mode=os.F_OK | os.X_OK)
+        path = shutil.which(arg, mode=os.F_OK | os.X_OK)
         if arg in builtin:
             sys.stdout.write(f"{arg} is a shell builtin\n")
         elif path:
