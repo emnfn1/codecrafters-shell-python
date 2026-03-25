323b231ef7e60ccaa5289a6ce34b33f5a66a40d9
Emin Furkan Safer <eminfurkan.safer@ogr.iu.edu.tr>
2026-03-25 19:59:44 +0300

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -15,7 +15,7 @@ def main():
             print(" ".join(user_input[1:]))
         elif user_input[0] == "type":
             if user_input[1] in ["echo", "type", "exit"]:
-                print(f"{user_input[1]} is shell builtin")
+                print(f"{user_input[1]} is a shell builtin")
             else:
                 path = shutil.which(user_input[1], mode = os.F_OK | os.X_OK)
                 if path:
