24fffb47a55314d35ba70469f55f727609989d17
Emin Furkan Safer <eminfurkan.safer@ogr.iu.edu.tr>
2026-03-25 19:24:50 +0300

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -4,11 +4,18 @@ import sys
 def main():
     while True:
         sys.stdout.write("$ ")
-        command = input()
+        user_input = input()
+        user_input = user_input.split()
+
         if command == "exit":
             break
         elif command.startswith("echo "):
             print(command[5:])
+        elif command == "type"
+            if "type" or "exit" or "echo" = user_input[1]:
+                print(f"{user_input[1]} is a shell builtin)
+            else:
+                print(f"{user_input[1]}: not found")
         else:
             print(f"{command}: command not found")
 
