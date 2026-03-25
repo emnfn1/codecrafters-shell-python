7650c8ac29d21b89c6e5b7419b43b2a918641926
Emin Furkan Safer <eminfurkan.safer@ogr.iu.edu.tr>
2026-03-25 19:05:55 +0300

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -8,6 +8,8 @@ def main():
         if command == "exit":
             break
         print(f"{command}: command not found")
+        if command.startswith("echo"):
+            print(command[5:])
 
 
 if __name__ == "__main__":
