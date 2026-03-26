1b886fcb435f93cb2062e8e3c2907d2f796ac5b7
Emin Furkan Safer <eminfurkan.safer@ogr.iu.edu.tr>
2026-03-26 16:17:28 +0300

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -4,20 +4,21 @@ import shutil
 import os
 import subprocess
 
+def cd_function(args):
+    if not os.path.isabs(args[0]):
+        return
+    elif not os.path.isdir(args[0]):
+        sys.stderr.write(f"cd: {args[0]}: No such file or directory\n")
+    else:
+        os.chdir(args[0])
 
 builtin = {
     "type": lambda args: custom_args(args),
     "exit": lambda args: sys.exit(0),
     "echo": lambda args: sys.stdout.write(f"{' '.join(args)}\n"),
     "pwd": lambda args: sys.stdout.write(f"{os.getcwd()}\n"),
-    "cd": cd_function(args):
-        if not os.path.abs(args[0]):
-            continue
-        elif not os.path.isdir(arg[0]):
-            sys.sys.stderr.write(f"cd: {args[0]}: No such file or directory\n")
-        else:
-            os.os.chdir(args[0]),
-        
+    "cd": cd_function,
+     
     }
 
 def custom_args(args):
