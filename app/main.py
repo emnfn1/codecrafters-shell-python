1266ea2a694e35db1382335a430ebc3687eea126
Emin Furkan Safer <eminfurkan.safer@ogr.iu.edu.tr>
2026-03-26 16:02:16 +0300

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -11,9 +11,8 @@ builtin = {
     "echo": lambda args: sys.stdout.write(f"{' '.join(args)}\n"),
     "pwd": lambda args: sys.stdout.write(f"{os.getcwd()}\n"),
     "cd": lambda args: sys.stderr.write(f"cd: {args[0]}: No such file or directory")
-    if os.path.exists(args[0]):
-        os.chidir(args[0])
-    else ,
+    if not os.path.exists(args[0])
+    else os.chidir(args[0]),
         
     }
 
